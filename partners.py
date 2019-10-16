c7e5b70813a0aa647bb7e99b8f0926be7b25a8ef
sunlu123456 <845534336@qq.com>
2019-10-16 09:18:52 +0800

    修改股东正则表达式，解决只显示一个股东的问题

--- partners.py
+++ partners.py
@@ -29,8 +29,8 @@ def export_partners(data_list,workbook):
         company_name = re.findall('<div class="company-name">(.*?\s*)<', _response)[0].strip()
         print('公司名称：' + company_name)
         # print(_response)
-        list = re.findall('<div class="stock-item">(.*\s*\n*.*\s*\n*.*\s*\n*)</div>\n?\s*</div>\n?\s*</div>\n?\s*</div>', _response)
-        # print(list)
+        list = re.findall('<div class="stock-item">(.*?\s*\n*.*?\s*\n*.*?\s*\n*)</div>\n?\s*</div>\n?\s*</div>\n?\s*</div>', _response)
+        print(len(list))
         if len(list) > 0:
             worksheet.write_merge(i + 1, i + len(list), 0, 0, j, style_merge)   # 序号
             worksheet.write_merge(i+1, i + len(list), 1, 1, company_name, style_merge)  # 合并公司名称单元格
@@ -83,24 +83,26 @@ def export_partners(data_list,workbook):
 
 if __name__ == '__main__':
    _response =  '''
-        <div id="partners" class="content-block"> <div class="block-title">
-                    股东信息
-                    <span class="block-num">1</span> <img onclick="collapse(this)" src="/material/theme/mobile/img/wxa-service/arrow_down.png" alt=""> </div> <div> <div class="stock-wrap"> <div class="stock-item"> <div class="stock-title"> <span > <a class="text-blue a-decoration" href="/pl_pc0e34dcaa71d657934f62079feb1bf5.html" >汤霞</a> </span><span class="company-status status-normal">自然人股东</span> </div> <div class="stock-content"> <div class="stock-row"> <div class="stock-col-left"> <div class="stock-subtitle">持股比例</div> <div class="stock-text">100%</div> </div> <div class="stock-col-right"> <div class="stock-subtitle">股东类型</div> <div class="stock-text">自然人股东</div> </div> </div> <div class="stock-row" style="margin-top: 20px;"> <div class="stock-col-left"> <div class="stock-subtitle">认缴出资额(万元)</div> <div class="stock-text">                                        -
+        股东信息
+                    <span class="block-num">3</span> <img onclick="collapse(this)" src="/material/theme/mobile/img/wxa-service/arrow_down.png" alt=""> </div> 
+                    <div> <div class="stock-wrap"> <div class="stock-item"> <div class="stock-title"> <span > <a class="text-blue a-decoration" >东莞市吉田焊接材料有限公司</a> </span><span class="company-status status-normal">企业法人</span> </div> <div class="stock-content"> <div class="stock-row"> <div class="stock-col-left"> <div class="stock-subtitle">持股比例</div> <div class="stock-text">51.00%</div> </div> <div class="stock-col-right"> <div class="stock-subtitle">股东类型</div> <div class="stock-text">企业法人</div> </div> </div> <div class="stock-row" style="margin-top: 20px;"> <div class="stock-col-left"> <div class="stock-subtitle">认缴出资额(万元)</div> <div class="stock-text">                                                                                -
                     </div> </div> <div class="stock-col-right"> <div class="stock-subtitle">认缴出资日期</div> <div class="stock-text">                                        -
+                                        </div> </div> </div> </div> </div> </div> <div class="stock-wrap"> <div class="stock-item"> <div class="stock-title"> <span > <a class="text-blue a-decoration" >深圳市吉田新电子有限公司</a> </span><span class="company-status status-normal">企业法人</span> </div> <div class="stock-content"> <div class="stock-row"> <div class="stock-col-left"> <div class="stock-subtitle">持股比例</div> <div class="stock-text">34.00%</div> </div> <div class="stock-col-right"> <div class="stock-subtitle">股东类型</div> <div class="stock-text">企业法人</div> </div> </div> <div class="stock-row" style="margin-top: 20px;"> <div class="stock-col-left"> <div class="stock-subtitle">认缴出资额(万元)</div> <div class="stock-text">                                                                                680<br/> </div> </div> <div class="stock-col-right"> <div class="stock-subtitle">认缴出资日期</div> <div class="stock-text">                                        -
+                                        </div> </div> </div> </div> </div> </div> <div class="stock-wrap"> <div class="stock-item"> <div class="stock-title"> <span > <a class="text-blue a-decoration" >珠海横琴吉田成长投资合伙企业（有限合伙）</a> </span><span class="company-status status-normal">其他投资者</span> </div> <div class="stock-content"> <div class="stock-row"> <div class="stock-col-left"> <div class="stock-subtitle">持股比例</div> <div class="stock-text">15.00%</div> </div> <div class="stock-col-right"> <div class="stock-subtitle">股东类型</div> <div class="stock-text">其他投资者</div> </div> </div> <div class="stock-row" style="margin-top: 20px;"> <div class="stock-col-left"> <div class="stock-subtitle">认缴出资额(万元)</div> <div class="stock-text">                                                                                300<br/> </div> </div> <div class="stock-col-right"> <div class="stock-subtitle">认缴出资日期</div> <div class="stock-text">                                        -
                                         </div> </div> </div> </div> </div> </div> </div> </div>
         '''
-   list = re.findall('<div class="stock-item">(.*\s*\n*.*\s*\n*.*\s*\n*)</div>\n?\s*</div>\n?\s*</div>\n?\s*</div>', _response)
+   list = re.findall('<div class="stock-item">(.*?\s*\n*.*?\s*\n*.*?\s*\n*)</div>\n?\s*</div>\n?\s*</div>\n?\s*</div>', _response)
 
    # list = re.findall(
    # '<div class="stock-item"> <div class="stock-title"> <span > <a class="text-blue a-decoration"( href=".*?")? >(\n*\s*.*?\s*\n*)</div>\n*\s*</div>\n*\s*</div>\n*\s*</div>', _response)
 
    print(list)
 
-   _response1 =  '''
-   <div id="partners" class="content-block"> <div class="block-title">
-                    股东信息
-                    <span class="block-num">1</span> <img onclick="collapse(this)" src="/material/theme/mobile/img/wxa-service/arrow_down.png" alt=""> </div> <div> <div class="stock-wrap"> <div class="stock-item"> <div class="stock-title"> <span > <a class="text-blue a-decoration" href="/pl_p5280f170c45532c4fbcbe4b1c9b4c3f.html" >朱成华</a> </span><span class="company-status status-normal">自然人股东</span> </div> <div class="stock-content"> <div class="stock-row"> <div class="stock-col-left"> <div class="stock-subtitle">持股比例</div> <div class="stock-text">100%</div> </div> <div class="stock-col-right"> <div class="stock-subtitle">股东类型</div> <div class="stock-text">自然人股东</div> </div> </div> <div class="stock-row" style="margin-top: 20px;"> <div class="stock-col-left"> <div class="stock-subtitle">认缴出资额(万元)</div> <div class="stock-text">                                                                                1000<br/> </div> </div> <div class="stock-col-right"> <div class="stock-subtitle">认缴出资日期</div> <div class="stock-text">                                        -
-                                        </div> </div> </div> </div> </div> </div> </div> </div>
-   '''
-   list1 = re.findall('<div class="stock-item">(.*\s*\n*.*\s*\n*.*\s*\n*)</div>\n?\s*</div>\n?\s*</div>\n?\s*</div>', _response1)
-   print(list1)
+   # _response1 =  '''
+   # <div id="partners" class="content-block"> <div class="block-title">
+   #                  股东信息
+   #                  <span class="block-num">1</span> <img onclick="collapse(this)" src="/material/theme/mobile/img/wxa-service/arrow_down.png" alt=""> </div> <div> <div class="stock-wrap"> <div class="stock-item"> <div class="stock-title"> <span > <a class="text-blue a-decoration" href="/pl_p5280f170c45532c4fbcbe4b1c9b4c3f.html" >朱成华</a> </span><span class="company-status status-normal">自然人股东</span> </div> <div class="stock-content"> <div class="stock-row"> <div class="stock-col-left"> <div class="stock-subtitle">持股比例</div> <div class="stock-text">100%</div> </div> <div class="stock-col-right"> <div class="stock-subtitle">股东类型</div> <div class="stock-text">自然人股东</div> </div> </div> <div class="stock-row" style="margin-top: 20px;"> <div class="stock-col-left"> <div class="stock-subtitle">认缴出资额(万元)</div> <div class="stock-text">                                                                                1000<br/> </div> </div> <div class="stock-col-right"> <div class="stock-subtitle">认缴出资日期</div> <div class="stock-text">                                        -
+   #                                      </div> </div> </div> </div> </div> </div> </div> </div>
+   # '''
+   # list1 = re.findall('<div class="stock-item">(.*\s*\n*.*\s*\n*.*\s*\n*)</div>\n?\s*</div>\n?\s*</div>\n?\s*</div>', _response1)
+   # print(list1)
